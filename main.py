d3badb380d4be791e949e21658c9382178b80fc0
[Aaron Lo] <[loaaron1337@gmail.com]>
2021-01-04 10:17:26 -0800

    chabge

---
+++ main.py
@@ -0,0 +1,18 @@
+#Midnight rider a text speech game
+#Game where text appears and you choose options
+
+
+def main():
+    pass
+# Show intro
+# Give player choices
+# Change enviroment based on choice and rng
+#Check if reached endgame
+
+
+
+if __name__ == '__main__':
+    if __name__ == '__main__':
+        ('PyCharm')
+
+
